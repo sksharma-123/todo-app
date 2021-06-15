fd5db4c15803dda7bac37e809aac244697fc62a0
sksharma123 <sauravjmsg@gmail.com>
2021-06-16 01:17:35 +0530

    Modified

--- web/settings.py
+++ web/settings.py
@@ -1,4 +1,4 @@
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+django_heroku.settings(locals())
